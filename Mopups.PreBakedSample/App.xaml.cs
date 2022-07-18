6f9b97cc04351c4c9899ee82a18af634a7e49fb9
Tyson Elliot Hooker <17778102+LuckyDucko@users.noreply.github.com>
2022-07-18 13:29:55 +1000

    Renamed to Prebaked as awaitable will probably be moved into the main repo.

---
+++ Mopups.PreBakedSample/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using SampleMaui.CSharpMarkup;
+
+namespace Mopups.PreBakedSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ Mopups.PreBakedSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Mopups.PreBakedSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
